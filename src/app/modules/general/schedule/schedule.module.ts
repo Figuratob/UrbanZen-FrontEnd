aa4cfceb5fe907e30a7b0595ad31c1da27b950ea
Marina <marina.tobler@gmail.com>
2020-02-18 10:13:52 +0200

    translation added to schedule

--- src/app/modules/general/schedule/schedule.module.ts
+++ src/app/modules/general/schedule/schedule.module.ts
@@ -3,17 +3,19 @@ import {CommonModule} from '@angular/common';
 import {ScheduleComponent} from './schedule.component';
 import {ScheduleRoutingModule} from './schedule-routing.module';
 import {TimetableModule} from '../../application/timetable/timetable.module';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
     ScheduleComponent,
 
   ],
-  imports: [
-    CommonModule,
-    ScheduleRoutingModule,
-    TimetableModule
-  ],
+    imports: [
+        CommonModule,
+        ScheduleRoutingModule,
+        TimetableModule,
+        TranslateModule
+    ],
   exports: [
     ScheduleComponent
   ]
